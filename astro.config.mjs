199836229e50f8e74c13f1fc16bf8ea55ededb29
C130AIR <2303912195@qq.com>
2026-07-28 18:05:30 +0800

    fix: correct base path to /C130AIR-Blog/ for GitHub Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -26,8 +26,8 @@ import { pluginCustomCopyButton } from "./src/plugins/expressive-code/custom-cop
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://kzx.icu/",
-	base: "/",
+	site: "https://c130air.github.io",
+	base: "/C130AIR-Blog/",
 	trailingSlash: "always",
 	integrations: [
 		tailwind({
